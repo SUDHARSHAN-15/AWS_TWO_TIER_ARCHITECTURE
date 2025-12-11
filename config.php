76038d98b2699ea4aaffc2a04fb6a80fda01debf
root <root@ip-10-0-0-84.us-west-2.compute.internal>
2025-12-11 16:50:38 +0000

    Hide RDS password for security

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ session_start();  // Important: Start session here for cart
 
 $host = 'database-1.cdmeihzpu5cy.us-west-2.rds.amazonaws.com';
 $user = 'admin';
-$pass = 'Nav12345';  // CHANGE THIS IMMEDIATELY AFTER TESTING!
+$pass = 'YOUR_RDS_PASSWORD_HERE';  // Set in environment or keep secret
 $db = 'testdb';
 
 $conn = new mysqli($host, $user, $pass, $db);
